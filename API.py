edae445a7a4b9156345505f754d17351fbf2c710
ColdEstrandiv <edwardostrandbergiv@gmail.com>
2024-02-25 12:36:49 -0500

    More endpoints made

--- API.py
+++ API.py
@@ -62,9 +62,58 @@ def user():
                 db.commit()
                 return f"{deletedUser.username} has been deleted", 200
 
-@app.route("/user/bookProgress")
+@app.route("/user/bookProgress", methods=["GET", "POST", "DELETE"])
 def get_user_bookProgresses():
-    ...
+    db = get_db()
+    userBookProgress = request.json
+
+    match request.method:
+        case "GET":
+            getUserBookProgess = db.query(BookProgress).where(BookProgress.id == userBookProgress["id"]).first()
+
+            if not getUserBookProgess:
+                return "BookProgress not found", 200
+            
+            else:
+                result ={
+                    "id": getUserBookProgess.id,
+                    "user": getUserBookProgess.user.username,
+                    "book": getUserBookProgess.book.title,
+                    "status": getUserBookProgess.status,
+                }
+
+                return jsonify(result), 200
+        
+        case "POST":
+            getUser = db.query(User).where(User.id == int(userBookProgress["userId"])).first()
+            getBook = db.query(Book).where(Book.id == int(userBookProgress["bookId"])).first()
+            newBookProgress = BookProgress(
+                user=getUser,
+                book=getBook
+            )
+            
+            if not getBook or not getUser:
+                return "Book or user not found", 200
+            
+            else:
+                db.add(newBookProgress)
+                db.commit()
+                return f"Bookprogress for {getBook.title} by {getUser.username}"
+        
+        case "DELETE":
+            deletedBookProgress = db.query(BookProgress).where(BookProgress.id == userBookProgress["id"]).first()
+            
+            # This is done to properly load the objs in the return fstring TODO: find a better way to load these
+            dbpUser = deletedBookProgress.user
+            dbpBook = deletedBookProgress.book
+
+            if not deletedBookProgress:
+                return "No progress to delete"
+            
+            else:
+                db.delete(deletedBookProgress)
+                db.commit()
+                return f"Bookprogress for {dbpBook.title} by {dbpUser.username} has been deleted", 200
 
 @app.route("/user/reads", methods=["GET", "POST", "DELETE"])
 def get_user_reads():
@@ -113,6 +162,7 @@ def get_user_reads():
             )
                 db.add(newReadSession)
                 db.commit()
+                return "Read Session Created", 200
 
         case "DELETE":  
             deletedRead = db.query(ReadingSession).where(ReadingSession.id == int(userRead["id"])).first()
@@ -125,15 +175,90 @@ def get_user_reads():
                 db.commit()
                 return f"Read id: {deletedRead.id} deleted", 200
 
-@app.route("/libraries/<userId>")
+@app.route("/libraries", methods = ["GET"])
 def get_libraries_by_user():
-    ...
+    db = get_db()
+    getLibrary = request.json
+    match request.method:
+        case "GET":
+            # TODO: make this grab all and not just the first one
+            library = db.query(Library).where(Library.userId == int(getLibrary["id"])).first()
+            libraryBooks = []
 
-@app.route("/library/<libraryId>")
+            if not library:
+                return "Library not found", 200
+
+            else:
+                for b in sorted(library.books, key=lambda b: b.author):
+                    libraryBooks.append(b.title, b.author)
+
+                result = {
+                    "Id": library.id,
+                    "name": library.name,
+                    "user": library.user.name,
+                    "books": libraryBooks
+                }
+                
+                return jsonify(result)
+
+
+@app.route("/library", methods=["GET", "POST", "DELETE", "PUT"])
 def get_library_by_id():
-    ...
+    db = get_db()
+    getLibrary = request.json
 
-@app.route("/book/<bookId>")
+    match request.method:
+        case "GET":
+            library = db.query(Library).where(Library.id == int(getLibrary["id"])).first()
+            libraryBooks = []
+
+            if not library:
+                return "Library not found", 200
+
+            else:
+                for b in sorted(library.books, key=lambda b: b.author):
+                    libraryBooks.append(b.title, b.author)
+
+                result = {
+                    "Id": library.id,
+                    "name": library.name,
+                    "user": library.user.name,
+                    "books": libraryBooks
+                }
+                
+                return jsonify(result)
+        
+        case "POST":
+            libraryUser = db.query(User).where(User.id == int(getLibrary["userId"])).first()
+
+            if not libraryUser:
+                return jsonify("User not found"), 200
+            
+            else:
+                newLibrary = Library(
+                    name=getLibrary["name"],
+                    user=libraryUser
+                )
+                
+                db.add(newLibrary)
+                db.commit()
+                return jsonify(f"{newLibrary.name} created for {libraryUser.username}"), 200
+
+        case "DELETE":
+            deletedLibrary = db.query(Library).where(Library.id == int(getLibrary["id"])).first()
+
+            if not deletedLibrary:
+                return jsonify("Library not found"), 200
+            
+            else:
+                db.delete(deletedLibrary)
+                db.commit()
+                return jsonify(f"{deletedLibrary.name} has been deleted"), 200
+        
+        case "PUT":
+            ...
+
+@app.route("/book")
 def get_book():
     # Show Book data, users reading book
     ...
